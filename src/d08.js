a71c7ef440cd7d3b50d1922b5511e53b7d9ef3bc
Alberto Bastos <albertobastos@gmail.com>
2018-12-08 09:36:01 +0100

    day 8

---
+++ src/d08.js
@@ -0,0 +1,63 @@
+console.time("d08");
+const rl = require("./utils").getInputRL("d08");
+
+function programReadLine(rl) {
+  let root;
+  rl.on("line", line => {
+    let items = line.split(" ").map(str => Number(str));
+    root = parseNode(items).node;
+  });
+
+  rl.on("close", () => {
+    console.log("Answer (part I):", sumMetadata(root));
+    console.log("Answer (part II):", nodeValue(root));
+  });
+}
+
+function parseNode(items) {
+  let childrenCount = items[0];
+  let metadataCount = items[1];
+
+  let children = [];
+  items = items.slice(2);
+  for (let i = 0; i < childrenCount; i++) {
+    let tmp = parseNode(items);
+    children.push(tmp.node);
+    items = tmp.itemsLeft;
+  }
+
+  metadata = items.slice(0, metadataCount);
+  items = items.slice(metadataCount);
+
+  return {
+    node: {
+      children: children,
+      metadata: metadata
+    },
+    itemsLeft: items
+  };
+}
+
+function sumMetadata(node) {
+  return (
+    node.metadata.reduce((acc, curr) => acc + curr, 0) +
+    node.children.reduce((acc, curr) => acc + sumMetadata(curr), 0)
+  );
+}
+
+function nodeValue(node) {
+  if (node.children.length < 1) {
+    // node without children, metadata items are literal values
+    return sumMetadata(node);
+  }
+
+  // node with children, metadata items are references to children nodes
+  return node.metadata.reduce(
+    (acc, index) =>
+      acc +
+      (node.children[index - 1] ? nodeValue(node.children[index - 1]) : 0),
+    0
+  );
+}
+
+programReadLine(rl);
